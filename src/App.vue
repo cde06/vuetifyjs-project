b206210149c3d859cfe716f14d9592d8b716c0e9
cde06 <cedric.debeaume@gmail.com>
2019-02-10 15:56:25 +0100

    Fix prettier conflict and update style on menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <!-- <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
@@ -14,21 +14,109 @@
     <v-content>
       <HelloWorld />
     </v-content>
+  </v-app>-->
+  <v-app id="inspire">
+    <v-navigation-drawer
+      id="navigation-drawer"
+      v-model="drawer"
+      dark
+      clipped
+      fixed
+      app
+    >
+      <v-list id="main-menu-list" dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <!-- <v-toolbar app fixed clipped-left> -->
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <div>
+        <img
+          src="@/assets/logo-gemaltox150.png"
+          style="margin-left:15px;padding:5px;float:left;"
+        />
+      </div>
+      <div
+        class="divider _ngcontent-rtu-1"
+        style="border-left: 1px solid #9e9e9e;height: 32px;margin-top: 5px; margin-left:14px;"
+      ></div>
+      <v-toolbar-title>Connectivity</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-icon>person</v-icon>
+        <!-- <v-avatar size="32px" tile>
+          <img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          />
+        </v-avatar>-->
+      </v-btn>
+    </v-toolbar>
+    <v-content id="main-content">
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <v-tooltip right>
+              <v-btn slot="activator" :href="source" icon large target="_blank">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  props: {
+    source: { type: String, default: undefined }
   },
-  data() {
-    return {
-      //
-    };
-  }
+  // components: {
+  //   HelloWorld
+  // },
+  // data() {
+  //   return {
+  //     //
+  //   };
+  // }
+  data: () => ({
+    drawer: null
+  })
 };
 </script>
+<style scoped>
+#main-content {
+  background-color: #e5e6e6;
+}
+#navigation-drawer {
+  background-color: #3c3c3c;
+}
+#main-menu-list {
+  margin-top: 16px;
+}
+</style>
